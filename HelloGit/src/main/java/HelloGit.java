2a298b67c5568ea3e6fef2d671a93496da35628e
xx <731955465@qq.com>
2020-06-02 10:41:05 +0800

    克隆更新。。。

--- HelloGit/src/main/java/HelloGit.java
+++ HelloGit/src/main/java/HelloGit.java
@@ -7,5 +7,6 @@ public class HelloGit {
         System.out.println("主干更新");
         System.out.println("分支更新");
         System.out.println("在线更新");
+        System.out.println("克隆更新。。。。");
     }
 }
